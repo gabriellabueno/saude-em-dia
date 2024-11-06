536502b89ee14f7321f3c0519b3e2f8b17d3fbe2
Sara Robles <sararoblesbarbosa2@gmail.com>
2024-11-05 21:52:07 -0300

    enum Nutricao atualizado

--- app/src/main/java/br/edu/fatec/diariosaude/util/Nutricao.java
+++ app/src/main/java/br/edu/fatec/diariosaude/util/Nutricao.java
@@ -3,17 +3,17 @@ package br.edu.fatec.diariosaude.util;
 public enum Nutricao {
 
     ABAIXO("IMC BAIXO! Abaixo do peso",
-            "Para ganhar peso de forma saudável, priorize alimentos nutritivos e calóricos. \nInclua uma fonte de proteína em todas as refeições, combine carboidratos complexos com gorduras saudáveis, faça refeições menores e lanches frequentes. \nConsuma alimentos ricos em ferro, cálcio e vitamina B12."),
+            "Priorize alimentos nutritivos e calóricos e inclua uma fonte de proteína em todas as refeições. \nCombine carboidratos complexos com gorduras saudáveis, faça refeições menores e lanches frequentes. \nConsuma alimentos ricos em ferro, cálcio e vitamina B12."),
     NORMAL("Peso normal.",
-            "Para manter o peso e uma nutrição equilibrada, encha metade do prato com vegetais e frutas. \nPrefira grãos integrais, inclua proteínas magras, use gorduras saudáveis e limite açúcares e sal."),
+            "Encha metade do prato com vegetais e frutas e prefira grãos integrais. \nInclua proteínas magras, use gorduras saudáveis e limite açúcares e sal."),
     SOBREPESO("IMC ALTO! Sobrepeso.",
-            "Para manter ou reduzir o peso, priorize alimentos de baixa densidade calórica e controle as porções. \nConsuma proteínas magras e laticínios com baixo teor de gordura, escolha grãos integrais, evite açúcares e gorduras processadas, e aumente a atividade física."),
+            "Priorize alimentos de baixa densidade calórica e controle as porções. \nConsuma proteínas magras e laticínios com baixo teor de gordura, escolha grãos integrais. \nEvite açúcares e gorduras processadas, e aumente a atividade física."),
     OBESIDADE1("IMC ALTO! Obesidade Grau I.",
-            "Para reduzir o peso de forma gradual e saudável, priorize alimentos ricos em fibras, reduza a ingestão calórica substituindo alimentos calóricos por opções saudáveis, prefira fontes magras de proteína, evite gorduras e açúcares processados, aumente a hidratação com água e considere o acompanhamento de um nutricionista para um plano alimentar personalizado."),
+            "Priorize alimentos ricos em fibras, substitua alimentos calóricos por opções saudáveis. \nPrefira fontes magras de proteína, evite gorduras e açúcares processados. \nBeba mais água e considere o acompanhamento de um nutricionista para um plano alimentar personalizado."),
     OBESIDADE2("IMC ALTO! Obesidade Grau II.",
-            "Para reduzir o peso de forma gradual e saudável, consuma alimentos ricos em fibras, substitua opções calóricas por alternativas saudáveis e de baixo teor calórico, inclua proteínas magras, evite açúcares e gorduras processadas, aumente a ingestão de água e considere o acompanhamento de um nutricionista para um plano alimentar personalizado."),
+            "Consuma alimentos ricos em fibras, substitua opções calóricas por alternativas saudáveis. \nInclua proteínas magras, evite açúcares e gorduras processadas. \nBeba mais água e considere o acompanhamento de um nutricionista para um plano alimentar personalizado."),
     OBESIDADE3("IMC ALTO! Obesidade Grau III.",
-            "Para reduzir o peso de forma estruturada e supervisionada, consuma alimentos ricos em fibras e mantenha a hidratação; faça uma redução calórica rigorosa com opções leves, sempre sob supervisão; evite açúcares e gorduras e busque acompanhamento multidisciplinar com nutricionista e médico para um plano seguro, considerando tratamentos adicionais quando necessário.");
+            "Consuma alimentos ricos em fibras e mantenha a hidratação. \nFaça uma redução calórica rigorosa com opções leves, sempre sob supervisão. \nEvite açúcares e gorduras e busque acompanhamento multidisciplinar com nutricionista e médico.");
 
     private final String indice;
     private final String nutricao;
